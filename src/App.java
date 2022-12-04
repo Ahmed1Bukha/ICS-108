fe2fa2fea74ba4907a1d5a6eaf8234b777456acb
Ahmed_Bukha <xxbu7aamedxx@gmail.com>
2022-12-04 07:51:29 -0800

    Staged the main stuff

--- src/App.java
+++ src/App.java
@@ -1,9 +1,13 @@
 import java.util.Scanner;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.*;  
@@ -14,13 +18,36 @@ import java.io.IOException;
 import java.util.*;
 
 public class App extends Application {
+  Scene scene1, scene2;
  @Override // Override the start method in the Application class
  public void start(Stage primaryStage) {
- BorderPane bPane = new BorderPane();
- primaryStage.setTitle("Course offering");
- Scene scene = new Scene(bPane,1200,700);
- primaryStage.setScene(scene);
- primaryStage.show();
+  primaryStage.setTitle("My First JavaFX GUI");
+
+  //Scene 1 Ahmed's Work
+  
+  Button button1= new Button("Next");
+  BorderPane pane1 = new BorderPane();
+  
+  button1.setOnAction(e -> primaryStage.setScene(scene2));  
+  pane1.setPadding(new Insets(4,4,4,4));
+  pane1.setBottom(button1);
+  BorderPane.setAlignment(button1, Pos.TOP_RIGHT);
+  scene1= new Scene(pane1, 1400, 900);
+                 
+
+
+
+  //Scene 2 Abdulla's work
+  Label label2= new Label("This is the second scene");
+  Button button2= new Button("Go to scene 1");
+  button2.setOnAction(e -> primaryStage.setScene(scene1));
+  VBox layout2= new VBox(20);
+  layout2.getChildren().addAll(label2, button2);
+  scene2= new Scene(layout2,1200,700);
+          
+          
+  primaryStage.setScene(scene1);
+  primaryStage.show();
 
  }
 
