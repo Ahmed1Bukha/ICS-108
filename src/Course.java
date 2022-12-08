328f5988ec82049fe60dc13b809b04a9a7ceea07
Ahmed_Bukha <xxbu7aamedxx@gmail.com>
2022-12-08 08:07:21 +0300

    fixed the reading method

--- src/Course.java
+++ src/Course.java
@@ -1,4 +1,6 @@
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -51,53 +53,59 @@ public class Course {
         return this.title;
     }
 
-    public static List<Course> coursesPossible(String coursesPath, String finishedPath, String dgreePlanPath) {
-        List<Course> Courses = new ArrayList<Course>();
+     public static List<Course> coursesPossible(String coursesPath, String finishedPath, String dgreePlanPath) {
+      List<Course> Courses = new ArrayList<Course>();
 
-        try {
+      try {
 
-            Scanner finishedCourseScanner = new Scanner(new File(finishedPath));
+          Scanner finishedCourseScanner = new Scanner(new File(finishedPath));
 
-            List<String> finishedCourse = new ArrayList<String>();
-            List<String> courseAvailable = new ArrayList<String>();
-            // It will check every finished course.
+          List<String> finishedCourse = new ArrayList<String>();
+          List<String> courseAvailable = new ArrayList<String>();
+          // It will check every finished course.
 
-            String courseName = "";
-            while (finishedCourseScanner.hasNextLine()) {
+          String courseName = "";
+          while (finishedCourseScanner.hasNextLine()) {
 
-                courseName = finishedCourseScanner.nextLine().split(",")[0];
-                finishedCourse.add(courseName);
-                System.out.println((finishedCourse));
-            }
+              courseName = finishedCourseScanner.nextLine().split(",")[0];
+              finishedCourse.add(courseName);
+              System.out.println((finishedCourse));
+          }
 
-            Scanner courseOfferingScanner = new Scanner(new File(coursesPath));
-            Scanner degreePlanScanner = new Scanner(new File(dgreePlanPath));
+       
+          Scanner degreePlanScanner = new Scanner(new File(dgreePlanPath));
 
-            while (degreePlanScanner.hasNextLine()) {
-                String[] tempo = degreePlanScanner.nextLine().split(",");
-                if (finishedCourse.contains(tempo[2]) && !finishedCourse.contains(tempo[0])) {
-                    courseAvailable.add(tempo[0]);
-                }
-            }
-            System.out.println(courseAvailable);
+          while (degreePlanScanner.hasNextLine()) {
+              String[] tempo = degreePlanScanner.nextLine().split(",");
+              if (finishedCourse.contains(tempo[2]) && !finishedCourse.contains(tempo[0])) {
+              
+                  courseAvailable.add(tempo[0]);
+              }
+          }
+   
+          Scanner courseOfferingScanner = new Scanner(new File(coursesPath));
+          BufferedReader br = new BufferedReader(new FileReader(new File(coursesPath)));
+          String available;
+          while ((available =br.readLine())!= null) {
+              String tempo = available;
+           
+              if (courseAvailable.contains(tempo.split(",")[0].split("-")[0])) {
+                  String[] tempoArray = tempo.split(",");
+                  Courses.add(
+                          new Course(tempoArray[0], tempoArray[1], tempoArray[2], tempoArray[4], tempoArray[5],
+                                  tempoArray[6]));
 
-            while (courseOfferingScanner.hasNextLine()) {
-                String tempo = courseOfferingScanner.nextLine();
-                if (courseAvailable.contains(tempo.split(",")[0].split("-")[0])) {
-                    String[] tempoArray = tempo.split(",");
-                    Courses.add(
-                            new Course(tempoArray[0], tempoArray[1], tempoArray[2], tempoArray[4], tempoArray[5],
-                                    tempoArray[6]));
+              }
+          
+          }
+       
+          return Courses;
+      } catch (IOException e) {
+          System.out.println("An error occurred.");
+          e.printStackTrace();
+          return Courses;
+      }
 
-                }
-            }
-            System.out.println(Courses);
-            return Courses;
-        } catch (IOException e) {
-            System.out.println("An error occurred.");
-            e.printStackTrace();
-            return Courses;
-        }
+  }
 
-    }
 }
