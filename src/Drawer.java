7a1e180d618c618ac769ed70a9c7abb3fc48e1f0
Ahmed_Bukha <xxbu7aamedxx@gmail.com>
2022-12-16 10:32:55 -0800

    finished

--- src/Drawer.java
+++ src/Drawer.java
@@ -1,17 +1,23 @@
 import java.util.Calendar;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Drawer {
   
-  
+    private HBox calendar;  
+    private VBox vBaskest;
     private Stage stage;
-    private HBox calendar;
+    private Map<String,List<Course>> mapDays;
+    private List<Course> courseBasket;
 
         public Drawer(Stage stage){
             this.stage = stage;
@@ -39,5 +45,33 @@ public class Drawer {
                 // show the dialog
                 a1.show();
         }
+
+        public void setCourseBakset(List<Course> basketCourses){
+            this.courseBasket = basketCourses;
+        }
+
+        public List<Course> getCourseBasket(){
+            return this.courseBasket;
+        }
+
+
+        public void setMapDays(Map<String,List<Course>> mapDays){
+            this.mapDays = mapDays;
+        }
+
+        public Map<String,List<Course>> getMapDays(){
+            return this.mapDays;
+        }
+        public void setCalendar(HBox caleBox){
+            this.calendar = caleBox;
+        }
+        public HBox getCalendar (){
+            return this.calendar;
+        }
+        public void setVbasket(VBox box){
+            this.vBaskest = box;
+        }
+        public VBox getVBasket()
+        {return this.vBaskest;}        
 
 }
--- src/SecondPage.java
+++ src/SecondPage.java
@@ -31,40 +31,65 @@ public class SecondPage {
         List<Course> tuesday = new ArrayList<>();
         List<Course> wednsday = new ArrayList<>();
         List<Course> thursday = new ArrayList<>();
+        HBox labelDays = new HBox();
+        HBox centerPage = new HBox();
+        labelDays.setSpacing(110);
+        
+                labelDays.getChildren().addAll(components.itemCard("Sunday"),
+                
+                components.itemCard("Monday"),
+                components.itemCard("Tuesday"),
+                components.itemCard("Wedsnday"),
+                components.itemCard("Thursday")
+                );
 
         schuMap.put("Sunday",sunday);
         schuMap.put("Monday",monday);
         schuMap.put("Tuesday",tuesday);
         schuMap.put("Wednsday",wednsday);
         schuMap.put("Thursday",thursday);
-        ArrayList<List> days = new ArrayList<>();
-
-        days.add(sunday);
-        days.add(monday);
-        days.add(tuesday);
-        days.add(wednsday);
-        days.add(thursday);
-
-        List<Course> jadwal = new ArrayList<>();
+        drawer.setMapDays(schuMap);
+        VBox labalTimes = new VBox();
+        labalTimes.setSpacing(90);
+        VBox space = new VBox();
+        space.setPrefHeight(1);
+        space.setPadding(new Insets(0));
+        labalTimes.getChildren().addAll(
+            space,
+            components.itemCard("7:00"),
+            components.itemCard("8:00"),
+            components.itemCard("9:00"),
+            components.itemCard("10:00"),
+            components.itemCard("11:00"),
+            components.itemCard("12:00"),
+            components.itemCard("1:00"),
+            components.itemCard("2:00"),
+            components.itemCard("3:00"),
+            components.itemCard("4:00"),
+            components.itemCard("5:00")
+        );
         VBox coursesColumn = new VBox();
+        drawer.setCourseBakset(basketCourses);
         ScrollPane scrollPane2 = new ScrollPane();
         ScrollPane scrollPaneBasket = new ScrollPane();
-        HBox calendar = new HBox();
+        HBox calendar = new HBox(0);
         pane2.setStyle("-fx-background-color: gray");
         scrollPaneBasket.setStyle("-fx-background-color: gray");
         scrollPane2.setStyle("-fx-background-color: gray");
         calendar.setStyle("-fx-background-color: gray ; -fx-border-color: black; -fx-border-width: 3;");
-        scrollPane2.setPadding(new Insets(20, 0, 20, 0));
+        scrollPane2.setPadding(new Insets(0, 0, 0, 0));
         scrollPaneBasket.setPadding(new Insets(20, 0, 20, 0));
 
         calendar.setPrefWidth(800);
-        calendar.setMinHeight(2000);
-        calendar.setPadding(new Insets(0, 20, 0, 20));
+        calendar.setPrefHeight(2000);
+        calendar.setPadding(new Insets(0, 0, 0, 0));
         scrollPane2.setMaxWidth(900);
+        calendar.getChildren().addAll(labalTimes);
+        centerPage.getChildren().addAll(labalTimes,calendar);
+        scrollPane2.setContent(centerPage);
 
-        scrollPane2.setContent(calendar);
-
-        calendar.setPadding(new Insets(0));
+        
+        drawer.setCalendar(calendar);
         Button button2 = new Button("Main Page");
         button2.setOnAction(arg0);
         button2.setFont(new Font(25));
@@ -72,6 +97,7 @@ public class SecondPage {
         button2.setMaxWidth(500);
         button2.setStyle("-fx-background-color: black; -fx-text-fill: white;");
         button2.setPadding(new Insets(20));
+
         BorderPane.setAlignment(button2, Pos.CENTER);
 
         scrollPaneBasket.setMinWidth(300);
@@ -80,13 +106,14 @@ public class SecondPage {
         coursesColumn.setPadding(new Insets(20, 10, 20, 10));
         coursesColumn.setSpacing(10);
         scrollPaneBasket.setContent(coursesColumn);
-
-        System.out.println(basketCourses.size());
+        drawer.setVbasket(coursesColumn);
+      
 
         // pane2.setPadding(new Insets(30));
         pane2.setPadding(new Insets(15, 20, 15, 20));
 
         pane2.setBottom(button2);
+
 
         pane2.setCenter(scrollPane2);
         for (int i = 0; i < basketCourses.size(); i++) {
@@ -100,14 +127,15 @@ public class SecondPage {
                     components.itemCard(basketCourses.get(i).getTitle() + " " + basketCourses.get(i).getActivity()),
                     components.itemCard(basketCourses.get(i).getDays()),
                     // there is a problem with this button, I'll fix it
-                    components.AddRemoveBasket(basketCourses.get(i), jadwal, basketCourses, coursesColumn, courseCard,
+                    components.AddRemoveBasket(basketCourses.get(i),  basketCourses, coursesColumn, courseCard,
                             schuMap,
                             calendar, drawer));
 
             courseCard.setAlignment(Pos.CENTER);
             coursesColumn.getChildren().addAll(courseCard);
-
+          
         }
+        pane2.setTop(labelDays);
         scene2 = new Scene(pane2, 1200, 600);
         return scene2;
     }
